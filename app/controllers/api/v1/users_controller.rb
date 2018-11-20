a3736a9f1ac5b1993b4a3a84996db2df62a90c0a
Harry <harrychenca@gmail.com>
2018-11-20 14:57:31 +0800

    Add users#create with specs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: {
+        success: true
+      }, status: :created
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  render_views
 
+  context 'POST #create' do
+    it 'renders create jbuilder and 201 status code' do
+      user = FactoryBot.create(:user)
+
+      get :create, params: { user:
+        {
+          email: user.email
+        }
+      }
+
+      expected_response = {
+        success: true
+      }
+
+      expect(response.code).to eq('201')
+      expect(response.body).to eq(expected_response.to_json)
+    end
+  end
 end
