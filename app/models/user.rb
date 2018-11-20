df6aa7113a43540121be20349490fb5e5e167eb5
Harry <harrychenca@gmail.com>
2018-11-20 14:49:56 +0800

    Add empty controllers and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User
   include Mongoid::Document
 
   field :email, type: String
+  field :first_name, type: String
+  field :last_name, type: String
 
   has_many :sessions
   has_many :bookings
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      resources :sessions, only: [:new, :create]
+
+      resources :listings, only: [:create, :show] do
+        resources :bookings, only: [:index, :create]
+      end
+    end
+  end
 end
